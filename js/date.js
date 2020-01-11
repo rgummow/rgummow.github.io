85ae47d15ca98fffec5bff410d3f871d5c0b5ab8
rgummow <rachel_gummow@yahoo.com>
2020-01-11 13:58:02 -0700

    added dates

---
+++ js/date.js
@@ -0,0 +1,4 @@
+document.getElementById('copyrightyear').textContent= new Date().getFullYear();
+const options = {weekday: 'long', day:'numeric', month:'long', year: 'numeric'};
+document.getElementById('currentdate').textContent= new Date().toLocaleDateString('en-us', options);
+document.getElementById('lastmodified').textContent = document.lastModified;
