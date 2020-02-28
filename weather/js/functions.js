f0299dead1f3f6ebfefc21db710907a845f5b3f9
rgummow <rachel_gummow@yahoo.com>
2020-02-28 11:03:33 -0700

    changed condition

--- weather/js/functions.js
+++ weather/js/functions.js
@@ -30,7 +30,7 @@ document.addEventListener("DOMContentLoaded", function(){
     let hour="3";
     timeBall(hour);
     //Implement the current weather background image
-    let condition ="rain";
+    let condition ="snow";
     changeSummaryImage(condition);
     //Get weather json data
     // let weatherURL = "/rgummow.github.io/weather/js/idahoweather.json";
