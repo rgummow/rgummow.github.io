cd1972408ffaca3052eba5ec3421eba1a259918b
rgummow <rachel_gummow@yahoo.com>
2020-02-28 10:50:53 -0700

    added background image

--- weather/js/functions.js
+++ weather/js/functions.js
@@ -1,5 +1,17 @@
 'use strict';
 
+// var pageNav = document.querySelector('#page-nav');
+// var statusContainer = document.querySelector('#status');
+// var contentContainer = document.querySelector('#main-content');
+
+// var $ = document.querySelector.bind(document);
+// var $$ = document.querySelectorAll.bind(document);
+// // var pageNav = $('#page-nav');
+
+// var locStore = window.localStorage;
+// var sessStore = window.sessionStorage;
+// // locStore.setItem("itemLabel",valueVariable);
+
 /* *************************************
 *  Weather Site JavaScript Functions
 ************************************* */
@@ -15,9 +27,18 @@ document.addEventListener("DOMContentLoaded", function(){
     let feelTemp = document.getElementById('feelTemp');
     feelTemp.innerHTML = buildWC(speed, temp);
     // The Time Indictor function
-    let hour="one";
+    let hour="3";
     timeBall(hour);
+    //Implement the current weather background image
+    let condition ="fog";
+    changeSummaryImage(condition);
+    //Get weather json data
+    // let weatherURL = "/rgummow.github.io/weather/js/idahoweather.json";
+    // fetchWeatherData(weatherURL);
 });
+
+//Builds copyright date
+document.getElementById('copyrightyear').textContent= new Date().getFullYear();
 
 // Build the last modified date
 function buildModDate(){
@@ -75,7 +96,7 @@ function timeBall(hour){
     }
     
     // Find all hours that match the parameter and add the "ball" class
-    let hr = document.querySelectorAll("."+hour);
+    let hr = document.querySelectorAll(".i"+hour);
     for (let item of hr){
         item.classList.add("ball");
     }
@@ -84,7 +105,60 @@ function timeBall(hour){
 /* *************************************
 * Change Background Image
 ************************************* */
-
-function changeSummaryImage(weather){
-    
+function changeSummaryImage(condition){
+    let container = document.querySelector("#curWeather");
+    console.log(condition);
+    container.classList.add(condition);
 }
+
+// /* *************************************
+// *  Fetch Weather Data
+// ************************************* */
+// function fetchWeatherData(weatherURL){
+//     let cityName = 'Preston'; // The data we want from the weather.json file
+//     fetch(weatherURL)
+//     .then(function(response) {
+//     if(response.ok){
+//     return response.json();
+//     }
+//     throw new ERROR('Network response was not OK.');
+//     })
+//     .then(function(data){
+//       // Check the data object that was retrieved
+//       console.log(data);
+//       // data is the full JavaScript object, but we only want the preston part
+//       // shorten the variable and focus only on the data we want to reduce typing
+//       let p = data[cityName];
+  
+//       // **********  Get the location information  **********
+//       let locName = p.City;
+//       let locState = p.State;
+//       // Put them together
+//       let fullName = locName+', '+locState;
+//       // See if it worked, using ticks around the content in the log
+//       console.log(`fullName is: ${fullName}`);
+//       // Get the longitude and latitude and combine them to
+//       const latLong = p.properties.relativeLocation.geometry.coordinates[1] + ","+ p.properties.relativeLocation.geometry.coordinates[0];
+//       console.log(latLong);
+//       // a comma separated single string
+//       // Create a JSON object containing the full name, latitude and longitude
+//       // and store it into local storage.
+//       const prestonData = JSON.stringify({fullName,latLong});
+//       locStore.setItem("Preston,ID", prestonData);
+  
+//       // **********  Get the current conditions information  **********
+//       // As the data is extracted from the JSON, store it into session storage
+//       // Get the temperature data
+  
+  
+//       // Get the wind data 
+  
+  
+//       // Get the hourly data using another function - should include the forecast temp, condition icons and wind speeds. The data will be stored into session storage.
+  
+//     })
+//     .catch(function(error){
+//     console.log('There was a fetch problem: ', error.message);
+//     statusContainer.innerHTML = 'Sorry, the data could not be processed.';
+//     })
+//   }
